d5269c75dcc996f249dfa517ca6507d37fc5adbb
Portillo <portillon02@gmail.com>
2023-12-21 20:51:08 -0500

    cardholes shadow fixed

--- src/components/CardHoles.jsx
+++ src/components/CardHoles.jsx
@@ -3,23 +3,23 @@ export default function CardHoles({ px, py, size }) {
     <div className={`flex flex-row justify-around py-${py} px-${px}`}>
       <figure
         style={{ width: size, height: size }}
-        className="rounded-full shadow-inner shadow-slate-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
+        className="rounded-full shadow-inner shadow-gray-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
       ></figure>
       <figure
         style={{ width: size, height: size }}
-        className="rounded-full shadow-inner shadow-slate-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
+        className="rounded-full shadow-inner shadow-gray-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
       ></figure>
       <figure
         style={{ width: size, height: size }}
-        className="rounded-full shadow-inner shadow-slate-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
+        className="rounded-full shadow-inner shadow-gray-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
       ></figure>
       <figure
         style={{ width: size, height: size }}
-        className="rounded-full shadow-inner shadow-slate-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
+        className="rounded-full shadow-inner shadow-gray-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
       ></figure>
       <figure
         style={{ width: size, height: size }}
-        className="rounded-full shadow-inner shadow-slate-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
+        className="rounded-full shadow-inner shadow-gray-500 bg-[#F5F5F5] dark:bg-[#111111] dark:shadow-none dark:border-[#898788]"
       ></figure>
     </div>
   );
